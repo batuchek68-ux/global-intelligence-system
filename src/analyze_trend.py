64ab13a467d0a7ee293309c1017b0357361c8157
batuchek68-ux <batuchek68@gmail.com>
2026-06-15 00:44:39 +0800

    Update analyze_trend.py

--- src/analyze_trend.py
+++ src/analyze_trend.py
@@ -10,6 +10,14 @@ for item in news:
     title = item.get("title", "")
     words.extend(title.lower().split())
 
+# 👇 在这里加 stopwords 过滤
+stopwords = {"the", "is", "of", "and", "to", "in", "for", "on", "a"}
+
+words = [
+    w for w in words
+    if w not in stopwords and len(w) > 2
+]
+
 keywords = Counter(words).most_common(20)
 
 result = {
