02ba525c5db412e527b6bb7b6285d36a63e934ba
batuchek68-ux <batuchek68@gmail.com>
2026-06-15 10:26:17 +0800

    Update ai_summary.py

--- src/ai_summary.py
+++ src/ai_summary.py
@@ -1,1 +1,23 @@
+import json
+import os
 
+with open("data/cleaned_news.json","r",encoding="utf-8") as f:
+    news=json.load(f)
+
+summaries=[]
+
+for item in news[:50]:
+
+    title=item["original"]
+
+    summaries.append({
+        "title":title,
+        "summary":title
+    })
+
+os.makedirs("output",exist_ok=True)
+
+with open("output/summary.json","w",encoding="utf-8") as f:
+    json.dump(summaries,f,ensure_ascii=False,indent=2)
+
+print("summary generated")
